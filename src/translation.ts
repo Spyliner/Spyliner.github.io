9ff6373fe44b5a07897bcead57612bf6aaeebf0b
Edward <edwardsoen@gmail.com>
2023-11-27 14:47:53 +0700

    cleanup

--- src/translation.ts
+++ src/translation.ts
@@ -3,7 +3,6 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import XHR from "i18next-xhr-backend";
 
 import translationEn from "./locales/en/translation.json";
-import translationEs from "./locales/es/translation.json";
 
 i18n
   .use(XHR)
